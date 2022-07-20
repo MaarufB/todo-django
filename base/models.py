c2c7dfd337ceafe5c81a80c55320f5271703456c
Ma-aruf M. Burad <burad.m@AD.SPROBE.PH>
2022-07-20 13:41:24 +0800

    dumps data

--- base/models.py
+++ base/models.py
@@ -15,3 +15,27 @@ class Task(models.Model):
 
     # class Meta:
     #     ordering = ('-created',) #['complete']
+
+
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null = True, blank=True)
+    title =  models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    # deadline = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+class ProjectTask(models.Model):
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE, null= True, blank=True)
+    task_title = models.CharField(max_length=250)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.task_title
+
+    
